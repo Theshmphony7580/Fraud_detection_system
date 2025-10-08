78c2a1621a2f52ba544fa66565b6c9f3d1ec48cf
Rahul Varma <167012900+Meruem09@users.noreply.github.com.>
2025-10-09 00:23:34 +0530

    deleted few files

--- api_check.py
+++
@@ -1,5 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-print("Gemini key:", os.getenv("GEMINI_API_KEY"))
--- data_split.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("synthetic_scam_dataset_clean.csv")
-
-# Count actual samples per class
-print(df["ScamLabel"].value_counts())
-
-# Percentage distribution
-print(df["ScamLabel"].value_counts(normalize=True) * 100)
